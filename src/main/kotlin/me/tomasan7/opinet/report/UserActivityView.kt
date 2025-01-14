dc6d579005f37c55995afc1ea08cd4727acf2d5a
Tomasan7 <tomashula06@gmail.com>
2025-01-14 11:34:26 +0100

    Add post uploading to user activity

--- src/main/kotlin/me/tomasan7/opinet/report/UserActivityView.kt
+++ src/main/kotlin/me/tomasan7/opinet/report/UserActivityView.kt
@@ -2,6 +2,7 @@ package me.tomasan7.opinet.report
 
 import me.tomasan7.opinet.comment.CommentTable
 import me.tomasan7.opinet.database.View
+import me.tomasan7.opinet.post.PostTable
 import me.tomasan7.opinet.user.UserTable
 import me.tomasan7.opinet.vote.VoteTable
 import org.jetbrains.exposed.sql.Expression
@@ -10,16 +11,17 @@ import org.jetbrains.exposed.sql.alias
 import org.jetbrains.exposed.sql.countDistinct
 
 /*
-SELECT U.ID, COUNT(DISTINCT VOTE.ID) + COUNT(DISTINCT COMMENT.ID) AS activity
+SELECT U.ID, COUNT(DISTINCT VOTE.ID) + COUNT(DISTINCT COMMENT.ID) + COUNT(DISTINCT POST.ID) AS activity
 FROM "user" U
          LEFT JOIN VOTE ON U.ID = VOTE.USER_ID
          LEFT JOIN COMMENT ON U.ID = COMMENT.AUTHOR_ID
+         LEFT JOIN POST ON U.ID = POST.AUTHOR_ID
 GROUP BY U.ID;
 */
 
 private const val ACTIVITY_SUM_ALIAS = "activity"
 private val ACTIVITY_SUM_EXPR = Expression.build {
-    (VoteTable.id.countDistinct() + CommentTable.id.countDistinct()).alias(ACTIVITY_SUM_ALIAS)
+    (VoteTable.id.countDistinct() + CommentTable.id.countDistinct() + PostTable.id.countDistinct()).alias(ACTIVITY_SUM_ALIAS)
 }
 
 /**
@@ -27,7 +29,7 @@ private val ACTIVITY_SUM_EXPR = Expression.build {
  */
 object UserActivityView : View(
     name = "user_activity",
-    select = (UserTable leftJoin VoteTable leftJoin CommentTable)
+    select = (UserTable leftJoin VoteTable leftJoin CommentTable leftJoin PostTable)
         .select(UserTable.id, ACTIVITY_SUM_EXPR)
         .groupBy(UserTable.id)
 )
